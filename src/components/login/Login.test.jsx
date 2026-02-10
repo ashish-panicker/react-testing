27bfae1584d6d206afbff6865c7618d24be0c1cd
ashish-panicker <ashish.s.panicker@proton.me>
2026-02-10 13:05:52 +0530

    Test: Login Button click tested

--- src/components/login/Login.test.jsx
+++ src/components/login/Login.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
-import { describe } from 'vitest'
+import { describe, expect } from 'vitest'
 import Login from './Login'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
 
 describe('Login Component', function () {
 	it('renders all elements', function () {
@@ -24,5 +26,36 @@ describe('Login Component', function () {
 		expect(submitButton).toHaveClass('bg-indigo-600')
 		expect(submitButton).toHaveClass('hover:bg-indigo-700')
 		expect(submitButton).toHaveClass('rounded-md')
+	})
+
+	it('updates input fields and submits the form', async function () {
+		const user = userEvent.setup()
+		const consoleSpy = vi.spyOn(console, 'log')
+
+		render(<Login />)
+
+		const emailInput = screen.getByLabelText(/email address/i)
+		const passwordInput = screen.getByLabelText(/password/i)
+		const button = screen.getByRole('button', { name: /sign in/i })
+
+		expect(emailInput).toBeInTheDocument()
+		expect(passwordInput).toBeInTheDocument()
+		expect(button).toBeInTheDocument()
+
+		// simulate key inputs
+		await user.type(emailInput, 'ashish@gmail.com')
+		await user.type(passwordInput, 'password')
+
+		expect(emailInput).toHaveValue('ashish@gmail.com')
+		expect(passwordInput).toHaveValue('password')
+
+		await user.click(button)
+
+		expect(consoleSpy).toHaveBeenCalledWith('Logging in with:', {
+			email: 'ashish@gmail.com',
+			password: 'password',
+		})
+
+        consoleSpy.mockRestore()
 	})
 })
